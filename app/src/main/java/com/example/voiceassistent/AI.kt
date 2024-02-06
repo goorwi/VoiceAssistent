f8f178d03043f085c8e6bbfa1334d4d7799835b2
goorwi <russkih.vlad123@gmail.com>
2024-02-06 23:47:00 +0300

    Commit after second lab

--- app/src/main/java/com/example/voiceassistent/AI.kt
+++ app/src/main/java/com/example/voiceassistent/AI.kt
@@ -1,24 +1,88 @@
 package com.example.voiceassistent
 
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.util.Calendar
+import java.util.Date
 import java.util.Locale
+import java.util.concurrent.atomic.LongAccumulator
 
 class AI {
-    val answers = mapOf<String, String>(
+    enum class timeAnswer {
+        day,
+        hour,
+        dayOfWeek,
+        timeToDay
+    }
+
+    val answers = mapOf(
         "Привет" to "Приветик",
-        "Чем занимаешься?" to "Отвечаю на вопросы",
-        "Что делаешь?" to "Отвечаю на вопросы",
-        "Как дела?" to "Неплохо",
-        "Ответишь на вопросик?" to "Конечно",
-        "Любишь гранатовый сок?" to "Хоть я и робот, но люблю"
+        "Чем занимаешься" to "Отвечаю на вопросы",
+        "Что делаешь" to "Отвечаю на вопросы",
+        "Как дела" to "Неплохо",
+        "Ответишь на вопросик" to "Конечно",
+        "Любишь гранатовый сок" to "Хоть я и робот, но люблю",
+        "Какой сегодня день" to getTimeAnswer(timeAnswer.day, "null"),
+        "Который час" to getTimeAnswer(timeAnswer.hour, "null"),
+        "Какой день недели" to getTimeAnswer(timeAnswer.dayOfWeek, "null"),
     )
 
     fun getAnswer(text: String): String {
-        var result : String? = null
+        var result: String? = null
         for ((x, y) in answers) {
-            if (text.contains(x, ignoreCase = true))
+            if (text.contains("сколько дней до", ignoreCase = true)) {
+                result = getTimeAnswer(timeAnswer.timeToDay, text.substring(text.lastIndexOf(" ") + 1))
+            }
+            else if (text.contains(x, ignoreCase = true))
                 result = y
         }
         if (result == null) return "Вопрос понял. Думаю..."
         else return result
+    }
+
+    private fun getTimeAnswer(type: Enum<timeAnswer>, date: String): String {
+        when (type) {
+            timeAnswer.day -> {
+                return android.text.format.DateFormat.format("dd.MM.yyyy", Calendar.getInstance())
+                    .toString()
+            }
+
+            timeAnswer.hour -> {
+                return android.text.format.DateFormat.format("HH:mm", Calendar.getInstance()).toString()
+            }
+
+            timeAnswer.dayOfWeek -> {
+                return android.text.format.DateFormat.format("EEEE", Calendar.getInstance()).toString()
+            }
+
+            timeAnswer.timeToDay -> {
+                    val targetDate = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).parse(date)
+                    if (targetDate != null) {
+                        val currentDate = Date()
+                        val differenceInMillis = targetDate!!.time - currentDate.time
+                        val daysDifference = differenceInMillis / (24 * 60 * 60 * 1000)
+
+                        return if (daysDifference >= 0) {
+                            "До ${
+                                SimpleDateFormat(
+                                    "dd.MM.yyyy",
+                                    Locale.getDefault()
+                                ).format(targetDate!!)
+                            } осталось $daysDifference дней"
+                        } else {
+                            "Уже прошло"
+                        }
+                    } else {
+                        return "Вы не указали конечную дату. Пожалуйста, уточните дату в формате dd.MM.yyyy."
+                    }
+            }
+
+            else -> {
+                return "Вопрос понял. Думаю..."
+            }
+        }
     }
 }
--- app/src/main/java/com/example/voiceassistent/MainActivity.kt
+++ app/src/main/java/com/example/voiceassistent/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.voiceassistent
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private val sendButton: Button by lazy { findViewById<Button>(R.id.sendButton) }
     private val questionField: EditText by lazy { findViewById<EditText>(R.id.questionField) }
     private val chatWindow: TextView by lazy { findViewById<TextView>(R.id.chatWindow) }
+
+    lateinit var textToSpeech: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         sendButton.setOnClickListener {
             onSend()
         }
+
+        textToSpeech = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
+            it ->
+            if (it != TextToSpeech.ERROR) {
+                textToSpeech.language = Locale.getDefault()
+            }
+        })
     }
 
     private fun onSend() {
@@ -28,6 +39,10 @@ class MainActivity : AppCompatActivity() {
         val answer = AI().getAnswer(text)
         chatWindow.append(">> $text\n")
         chatWindow.append("<< $answer\n")
+
+        questionField.text.clear()
+
+        //textToSpeech.speak(answer, TextToSpeech.QUEUE_FLUSH, null, null)
 
         dismissKeyboard()
     }
